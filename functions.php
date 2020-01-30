af147dbfe46c2f5dc2dbfc07a94e2418fde441f5
Kiran Prajapati <officeofkiran@gmail.com>
2020-01-30 16:11:18 +0530

    2.0.3

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 // Define some constants
-if (!defined('WHOOP_VERSION')) define('WHOOP_VERSION', '2.0.2');
+if (!defined('WHOOP_VERSION')) define('WHOOP_VERSION', '2.0.3');
 
 // Call Whoop
 require_once( dirname( __FILE__ ) . '/includes/class-whoop.php' );
